5b5e133eb823bc56730f54c40bed39f795e96408
HaiderRe <haider.rehawi1@gmail.com>
2022-10-06 12:01:07 +0100

    Update byrefbyval.py

--- byrefbyval.py
+++ byrefbyval.py
@@ -1,14 +1,18 @@
-def fun(x, y):
-    x[0] = 2
-    y = 1
+# def fun(x, y):
+#     x[0] = 2
+#     y = 1
 
-mylist = [1,2,3,4]
-mytotal = 0
-print(mylist[0])
-print(mytotal)
-fun(mylist, mytotal)
-print(mylist[0], mytotal)
-
-
+# mylist = [1,2,3,4]
+# mytotal = 0
+# print(mylist[0])
+# print(mytotal)
+# fun(mylist, mytotal)
+# print(mylist[0], mytotal)
 
 
+ourlist = [2,3,-5,5]
+total = 0
+for x in ourlist:
+    total += x;
+
+print(total);
