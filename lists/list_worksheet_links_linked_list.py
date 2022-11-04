c8d59a74007c8c13584b207ff9e37023e6f351b2
HaiderRe <haider.rehawi1@gmail.com>
2022-11-04 11:45:52 +0000

    Update list_worksheet_links_linked_list.py

--- lists/list_worksheet_links_linked_list.py
+++ lists/list_worksheet_links_linked_list.py
@@ -4,8 +4,11 @@ list2 = []
 for index in range(0, len(list1) -1):
  if(list1[index] >= 80 and list1[index] <=100):
     count = count + 1
+ else:
+    list2.append(list1[index])
 print("number of integers in range 80-100" , count)
-for i in range(0,len(list1) - 1): 
-    if(not(list1[index] >= 80 and (list1[index] <=100))):
-        list2.append(list1[index])
-print(list2)           
+print(list2)
+#for i in range(0,len(list1) - 1): 
+ #   if(not(list1[index] >= 80 and (list1[index] <=100))):
+      #  list2.append(list1[index])
+#print(list2)           
